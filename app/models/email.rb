824e1bf14b8d1ed28cc81870d09bf85a35086e25
Meg Gutshall <meghan.gutshall@gmail.com>
2021-02-16 01:32:46 -0500

    Add code comments

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,6 +1,7 @@
 class Email < ApplicationRecord
   belongs_to :person
 
+  # Remember to run the migrations before adding the enum attribute below
   enum email_type: [:home, :work, :other] # implicit code
   # enum email_type; { home: 0, work: 1, other: 2 } # explicit code
 end
--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,6 +1,7 @@
 class Phone < ApplicationRecord
   belongs_to :person
 
+  # Remember to run the migrations before adding the enum attribute below
   enum phone_type: [:mobile, :work, :home] # implicit code
   # enum phone_type: { mobile: 0, work: 1, home: 2 } # explicit code
 end
