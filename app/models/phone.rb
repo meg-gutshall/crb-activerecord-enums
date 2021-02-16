6035dbd1b639e80fb9636a1a56766442c42094c4
Meg Gutshall <meghan.gutshall@gmail.com>
2021-02-16 01:19:00 -0500

    Add enum attribute to phone

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,3 +1,6 @@
 class Phone < ApplicationRecord
   belongs_to :person
+
+  enum phone_type: [:mobile, :work, :home] # implicit code
+  # enum phone_type: { mobile: 0, work: 1, home: 2 } # explicit code
 end
